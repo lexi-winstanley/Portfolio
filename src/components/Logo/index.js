1a79f98ff122cb7d82a78653f1b75a8a88575468
lexi-winstanley <50344054+lexi-winstanley@users.noreply.github.com>
2020-01-15 14:20:12 -0800

    colors and icons for hexagons updated

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -4,7 +4,7 @@ import './style.css';
 const Logo = props => {
     return (
         <div className="logoContainer">
-            <img className="navigationLogo" src="images/LogoBlack.svg" alt="logo showing white capital 'L' with thin white border"/>
+            <img className="navigationLogo" src="images/logoThin.svg" alt="logo showing white capital 'L' with thin white border"/>
         </div>
     )
 };
--- src/constants/hexTiles.js
+++ src/constants/hexTiles.js
@@ -5,7 +5,7 @@ export default {
             id: '1',
             imageSrc: '',
             imageAlt: '',
-            background: '#fd005f',
+            background: '#FC354C',
             title: ''
         },
         {
@@ -13,7 +13,7 @@ export default {
             id: '2',
             imageSrc: '',
             imageAlt: '',
-            background: '#f8a7d7',
+            background: '#041232',
             title: ''
         },
         {
@@ -21,7 +21,7 @@ export default {
             id: '3',
             imageSrc: '',
             imageAlt: '',
-            background: '#f4841a',
+            background: '#0072bb',
             title: ''
         },
         {
@@ -29,152 +29,152 @@ export default {
             id: '4',
             imageSrc: '',
             imageAlt: '',
-            background: '#a8c7c7',
+            background: '#cbc5ef',
             title: ''
         },
         {
             id: '5',
+            imageSrc: '',
+            imageAlt: '',
+            background: '#0cb2ba',
+            title: ''
+        },
+        {
+            id: '6',
             imageSrc: 'images/burger.svg',
             imageAlt: 'double decker burger icon',
             background: '#45bce5',
             title: 'Eat Da Burger'
         },
         {
-            id: '6',
-            imageSrc: '',
-            imageAlt: '',
-            background: '#45bce5',
-            title: 'Three Score Development'
-        },
-        {
             //cutoff
             id: '7',
             imageSrc: '',
             imageAlt: '',
-            background: '#a8a1a7',
+            background: '#FC354C',
             title: ''
         },
         {
             id: '8',
-            imageSrc: 'images/greenCrystal.svg',
-            imageAlt: 'green crystal icon',
-            background: 'rgba(234, 162, 4, .5)',
-            title: 'Crystal Counting'
-        },
-        {
-            id: '9',
-            imageSrc: 'images/gifGenerator.svg',
-            imageAlt: 'gif generator icon',
-            background: '#21202c',
-            title: 'GIF Generator'
-        },
-        {
-            id: '10',
-            imageSrc: 'images/bri-spy.svg',
-            imageAlt: 'bri-spy icon',
-            background: '#45bce5',
-            title: 'bri-SPY'
-        },
-        {
-            //cutoff
-            id: '11',
-            imageSrc: '',
-            imageAlt: '',
-            background: '#fd005f',
-            title: ''
-        },
-        {
-            id: '12',
-            imageSrc: 'images/friendFinder.svg',
-            imageAlt: 'friend finder icon',
-            background: '#45bce5',
-            title: 'Friend Finder'
-        },
-        {
-            id: '13',
-            imageSrc: '',
-            imageAlt: '',
-            background: '#fd005f',
-            title: 'Something Borrowed'
-        },
-        {
-            //cutoff
-            id: '14',
-            imageSrc: '',
-            imageAlt: '',
-            background: '#f8a1f7',
-            title: ''
-        },
-        {
-            id: '15',
-            imageSrc: '',
-            imageAlt: '',
-            background: '#fd005f',
-            title: 'Chapter Too Travel Blog'
-        },
-        {
-            id: '16',
-            imageSrc: '',
-            imageAlt: '',
-            background: '#f844f7',
-            title: ''
-        },
-        {
-            id: '17',
-            imageSrc: '',
-            imageAlt: '',
-            background: '#11f7f7',
-            title: ''
-        },
-        {
-            //cutoff
-            id: '18',
-            imageSrc: '',
-            imageAlt: '',
-            background: '#fd005f',
-            title: ''
-        },
-        {
-            id: '19',
-            imageSrc: '',
-            imageAlt: '',
-            background: '#0072bb',
-            title: 'Urban Prairie Homes'
-        },
-        {
-            id: '20',
-            imageSrc: 'images/sawblade.svg',
-            imageAlt: 'saw blade icon',
-            background: 'rgba(234, 162, 4, .5)',
-            title: 'Woodworking Trivia'
-        },
-        {
-            //cutoff
-            id: '21',
-            imageSrc: '',
-            imageAlt: '',
-            background: '#43ebc3',
-            title: ''
-        },
-        {
-            id: '22',
             imageSrc: 'images/pawPrint.svg',
             imageAlt: 'dog paw print icon',
             background: '#b5ca30',
             title: 'Dog Breed Guessing Game'
         },
         {
+            id: '9',
+            imageSrc: 'images/packIt.svg',
+            imageAlt: '',
+            background: '#680a4d',
+            title: 'Pack It!'
+        },
+        {
+            id: '10',
+            imageSrc: 'images/gifGenerator.svg',
+            imageAlt: 'gif generator icon',
+            background: '#041232',
+            title: 'GIF Generator'
+        },
+        {
+            //cutoff
+            id: '11',
+            imageSrc: '',
+            imageAlt: '',
+            background: '#041232',
+            title: ''
+        },
+        {
+            id: '12',
+            imageSrc: 'images/urbanPrairieWheat.svg',
+            imageAlt: '',
+            background: '#0072bb',
+            title: 'Urban Prairie Homes'
+        },
+        {
+            id: '13',
+            imageSrc: 'images/swissFavicon.svg',
+            imageAlt: '',
+            background: '#b5ca30',
+            title: 'Swiss Memory Game'
+        },
+        {
+            //cutoff
+            id: '14',
+            imageSrc: '',
+            imageAlt: '',
+            background: '#cbc5ef',
+            title: ''
+        },
+        {
+            id: '15',
+            imageSrc: 'images/friendFinder.svg',
+            imageAlt: 'friend finder icon',
+            background: '#680a4d',
+            title: 'Friend Finder'
+        },
+        {
+            id: '16',
+            imageSrc: 'images/threeScoreDev.svg',
+            imageAlt: '',
+            background: '#cbc5ef',
+            title: 'Three Score Development'
+        },
+        {
+            id: '17',
+            imageSrc: 'images/greenCrystal.svg',
+            imageAlt: 'green crystal icon',
+            background: '#FC354C',
+            title: 'Crystal Counting'
+        },
+        {
+            //cutoff
+            id: '18',
+            imageSrc: '',
+            imageAlt: '',
+            background: '#0cb2ba',
+            title: ''
+        },
+        {
+            id: '19',
+            imageSrc: 'images/bri-spy.svg',
+            imageAlt: 'bri-spy icon',
+            background: '#FC354C',
+            title: 'bri-SPY'
+        },
+        {
+            id: '20',
+            imageSrc: 'images/chapterToo.svg',
+            imageAlt: '',
+            background: '#041232',
+            title: 'Chapter Too Travel Blog'
+        },
+        {
+            //cutoff
+            id: '21',
+            imageSrc: '',
+            imageAlt: '',
+            background: '#0cb2ba',
+            title: ''
+        },
+        {
+            id: '22',
+            imageSrc: 'images/train.svg',
+            imageAlt: '',
+            background: '#cbc5ef',
+            title: 'Train Schedule'
+        },
+        {
             id: '23',
-            imageSrc: 'images/crystalBall.svg',
-            imageAlt: 'crystal ball icon',
+            imageSrc: 'images/somethingBorrowed.svg',
+            imageAlt: '',
             background: '#45bce5',
-            title: 'Letter Guessing Game'
+            title: 'Something Borrowed'
         },
         {
             id: '24',
-            imageSrc: '',
+            imageSrc: 'images/threeScoreRub.svg',
             imageAlt: '',
-            background: '#43ebc3',
+            background: '#0072bb',
             title: 'Three Score Rub'
         },
         {
@@ -186,8 +186,38 @@ export default {
             title: ''
         },
         {
+            id: '26',
+            imageSrc: 'images/sawblade.svg',
+            imageAlt: 'saw blade icon',
+            background: '#0cb2ba',
+            title: 'Woodworking Trivia'
+        },
+        {
+            id: '27',
+            imageSrc: 'images/crystalBall.svg',
+            imageAlt: 'crystal ball icon',
+            background: '#680a4d',
+            title: 'Letter Guessing Game'
+        },
+        {
             //cutoff
-            id: '25',
+            id: '28',
+            imageSrc: '',
+            imageAlt: '',
+            background: '#041232',
+            title: ''
+        },
+        {
+            //cutoff
+            id: '29',
+            imageSrc: '',
+            imageAlt: '',
+            background: '#FC354C',
+            title: ''
+        },
+        {
+            //cutoff
+            id: '30',
             imageSrc: '',
             imageAlt: '',
             background: '#b5ca30',
@@ -195,18 +225,10 @@ export default {
         },
         {
             //cutoff
-            id: '25',
+            id: '31',
             imageSrc: '',
             imageAlt: '',
-            background: '#b5ca30',
-            title: ''
-        },
-        {
-            //cutoff
-            id: '25',
-            imageSrc: '',
-            imageAlt: '',
-            background: '#b5ca30',
+            background: '#45bce5',
             title: ''
         }
         //end on multiple of 7n+3 or 7n
