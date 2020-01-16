acbefc87511f6fe5c9a0121fb1efcf0af00814c6
lexi-winstanley <50344054+lexi-winstanley@users.noreply.github.com>
2020-01-16 08:54:35 -0800

    Updates

---
+++ precache-manifest.88ca058cf65bb450d381b71fd4d1dfa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "752858bada0124ded6e1e756b0d1e77a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65f2636387cb5db15310",
+    "url": "/static/css/main.8f0bbaa4.chunk.css"
+  },
+  {
+    "revision": "72b0f18d41e7cc3b8273",
+    "url": "/static/js/2.c707be12.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.c707be12.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65f2636387cb5db15310",
+    "url": "/static/js/main.1ecc883a.chunk.js"
+  },
+  {
+    "revision": "45bc0b7821cb54ab2596",
+    "url": "/static/js/runtime-main.cd10a802.js"
+  }
+]);
