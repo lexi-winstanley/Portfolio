0562c20116f8274ca3f4f965762215d42ac50781
lexi-winstanley <50344054+lexi-winstanley@users.noreply.github.com>
2020-02-26 16:47:05 -0800

    landing page button working with hover effect

--- src/components/FullPageIntro/index.js
+++ src/components/FullPageIntro/index.js
@@ -16,9 +16,11 @@ const FullPageIntro = props => {
            props.introVisible === true ?
            <div className={opacityClasses}>
             <h2 className="textTitleCentered"> Hi, I'm Lexi, a detail-oriented Front End Web Developer known for delivering responsive, cross browser compatible applications that balance performance and design with overall user experience. Certificate in Full Stack Web Development from the University of Washington and strong skills in HTML, CSS and JavaScript.</h2>
+               <div id="hoverEffect">
                <button style={{backgroundColor: "#0F0F0D", color: "#f8f7f7"}} className="coloredButton bounce" onClick={fadeFromView}>
                    Enter Site
                </button>
+               </div>
         </div> : <></>
 
        )
