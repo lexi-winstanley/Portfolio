a0ccc4eab9fb4d33d99138e67848ce8444ca2394
lexi-winstanley <50344054+lexi-winstanley@users.noreply.github.com>
2020-08-14 15:16:12 -0700

    media queries for intro

--- src/components/ProjectPage/index.js
+++ src/components/ProjectPage/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './style.css';
 import Header from '../Header';
-import FlexColumnContainer from '../FlexColumnContainer';
-import EqualColumn from '../EqualColumn';
 import {Link, useParams} from 'react-router-dom';
 import projectList from '../../constants/projects';
 import ProjectInfo from "../ProjectInfo";
@@ -73,23 +71,6 @@ const ProjectPage = props => {
                     videoSrc={currentProject.videoSrc}
                     arrowTitle={true}
                 />
-                {/*<FlexColumnContainer>*/}
-                    {/*<EqualColumn>*/}
-                        {/*<h2 className="projectSubheading" style={styles.importedTextColor}>Description: </h2>*/}
-                        {/*<p className="projectText" style={styles.importedTextColor}>{currentProject.description}</p>*/}
-                        {/*<h2 className="projectSubheading" style={styles.importedTextColor}>Technical Information: </h2>*/}
-                        {/*<p className="projectText" style={styles.importedTextColor}>{currentProject.organization}</p>*/}
-                        {/*<h2 className="projectSubheading" style={styles.importedTextColor}>Technologies: </h2>*/}
-                        {/*<p className="projectText" style={styles.importedTextColor}>{currentProject.technologies}</p>*/}
-                        {/*<h2 className="projectExternalLink" style={styles.importedTextColor}><a href={currentProject.githubLink} style={styles.importedTextColor}>Github Repository</a></h2>*/}
-                        {/*<h2 className="projectExternalLink" style={styles.importedTextColor}><a href={currentProject.projectLink} style={styles.importedTextColor}>Live Project</a></h2>*/}
-                    {/*</EqualColumn>*/}
-                    {/*<EqualColumn>*/}
-                        {/*<div className="projectImageContainer">*/}
-                            {/*<img className="projectImage" src={currentProject.imageSrc} alt={currentProject.imageAlt}/>*/}
-                        {/*</div>*/}
-                    {/*</EqualColumn>*/}
-                {/*</FlexColumnContainer>*/}
             </div>
             </>
         );
